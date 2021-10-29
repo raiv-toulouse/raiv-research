7099a8c7446715b6b8deb59145130a0df4ddf4ef
raiv-toulouse@icam.fr <raiv-toulouse>
2021-10-29 17:57:32 +0200

    New version

--- src/random_picks_birdview.py
+++ src/random_picks_birdview.py
@@ -2,53 +2,58 @@
 # coding: utf-8
 
 """
-Used to get picking images from random position. The images go to 'success' or 'fail'  folders, depending if the gripper succeed or not
+Used to get picking images from random position. The images go to 'success/<rgb and depth>' or 'fail/<rgb and depth>' folders, depending if the gripper succeed or not
 
-- We need to establish a connection to the robot with the following comand:
-roslaunch ur_robot_driver ur3_bringup.launch robot_ip:=10.31.56.102 kinematics_config:=${HOME}/Calibration/ur3_calibration.yaml
-
-- Then, we need to activate moveit server:
-roslaunch ur3_moveit_config ur3_moveit_planning_execution.launch
+- We need to establish a connection to the robot with the following command:
+roslaunch raiv_libraries ur3_bringup_cartesian.launch robot_ip:=10.31.56.102 kinematics_config:=${HOME}/Calibration/ur3_calibration.yaml
 
 - Information from Arduino
 rosrun rosserial_arduino serial_node.py _port:=/dev/ttyACM0
 
 - launch program:
-python random_pick_birdview.py (in robot_controller/src)
+python random_pick_birdview.py <images_folder> <calibration_files_folder>
 
 """
-from PIL import Image as PILImage
-from sensor_msgs.msg import Image
 import rospy
 from datetime import datetime
-import random
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
-import cv2
-import os
-from raiv_camera_calibration.perspective_calibration import PerspectiveCalibration
-from raiv_libraries.robot_with_vaccum_gripper import Robot_with_vaccum_gripper
-from raiv_libraries.simple_image_controller import SimpleImageController
-from raiv_libraries.environment import Env_cam_bas
 from pathlib import Path
 import sys
+import cv2
+from raiv_camera_calibration.perspective_calibration import PerspectiveCalibration
+from raiv_libraries.robot_with_vaccum_gripper import Robot_with_vaccum_gripper
+from raiv_libraries.robotUR import RobotUR
+import geometry_msgs.msg as geometry_msgs
 
-CROP_WIDTH = 224
+#
+# Constants
+#
+CROP_WIDTH = 224 # Width and height for rgb and depth cropped images
 CROP_HEIGHT = 224
-HALF_CROP_WIDTH = int(CROP_WIDTH / 2)
-HALF_CROP_HEIGHT = int(CROP_HEIGHT / 2)
+Z_PICK_PLACE = 0.1  # Z coord to start pick or place movement
+X_OUT = 0.2  # XYZ coord where the robot is out of camera scope
+Y_OUT = 0.1
+Z_OUT = 0.1
 
-def goto_pixel_position(px, py):
-    # transposition des coordonnées pixel du point en coordonnées réelles dans le repère du robot
+def pixel_to_pose(px, py):
+    """ Transpose pixel coord to XYZ coord (in the base robot frame) and return the corresponding frame """
     xyz = dPoint.from_2d_to_3d([px, py])
-
-    # mouvement vers le point cible
     x = xyz[0][0] / 100
     y = xyz[1][0] / 100
-    robot.go_to_xyz_position(x, y, 0.3)
+    return geometry_msgs.Pose(
+        geometry_msgs.Vector3(x, y, Z_PICK_PLACE), RobotUR.tool_down_pose
+    )
+
+def save_images(folder, rgb, depth):
+    image_name = str(datetime.now()) + '.jpg'
+    for image_type, image in zip(['rgb', 'depth'], [resp.rgb, resp.depth]):
+        image_path = (parent_image_folder / folder / image_type / image_name).resolve()
+        cv2.imwrite(str(image_path), image)
 
 #
 # Main program
 #
+
+# Check if number of arguments is OK
 if len(sys.argv) != 3:
     print("Syntax : python random_picks_birdview.py <images_folder> <calibration_files_folder>")
     exit(1)
@@ -59,78 +64,49 @@ if not calibration_folder.exists():
     print("This folder doesn't exist : {}".format(sys.argv[2]))
     exit(2)
 
-# Create, if they don't exist, <images_folder>/success and <images_folder>/fail
+rospy.init_node('random_picks_birdview')
+# A UR robot with a vaccum gripper
+robot = Robot_with_vaccum_gripper()
+robot.go_to_xyz_position(X_OUT, Y_OUT, Z_OUT) # Go out of camera scope
+# We can now ask a service to get and process 3D images
+coord_service_name = 'In_box_coordService'
+rospy.wait_for_service(coord_service_name)
+coord_service = rospy.ServiceProxy('coord_service_name', get_coordServiceResponse)
+_ = coord_service('refresh', CROP_WIDTH, CROP_HEIGHT) # ask to refresh (i.e get a new image and find boxes)
+
+# Create, if they don't exist, <images_folder>/success/rgb, <images_folder>/success/depth,  <images_folder>/fail/rgb and <images_folder>/fail/depth folders
 parent_image_folder = Path(sys.argv[1])
-success_image_folder = parent_image_folder / 'success'
-Path.mkdir(success_image_folder, parents=True, exist_ok=True)
-fail_image_folder = parent_image_folder / 'fail'
-Path.mkdir(fail_image_folder, parents=True, exist_ok=True)
+for sf_folder in ['success','fail']:
+    for rd_folder in ['rgb', 'depth']:
+        folder = parent_image_folder / sf_folder / rd_folder
+        Path.mkdir(folder, parents=True, exist_ok=True)
 
-# Get an image from /usb_cam/image_raw topic
-image_controller = SimpleImageController()
-
-# A PrepectiveCalibration object to perform 2D => 3D conversion
+# A PerspectiveCalibration object to perform 2D => 3D conversion
 dPoint = PerspectiveCalibration(calibration_folder)
 
-# A UR robot with a vaccum gripper
-robot = Robot_with_vaccum_gripper(Env_cam_bas())
-
-# initialisation du noeud robotUr
-rospy.init_node('random_picks_birdview')
-
-# on remonte de 10cm au cas ou le robot serait trop bas
-robot.go_to_initial_position(3)
-
-# boucle du programme de prise d'image
+# Main loop to get image
 while True:
-
-    # on prend la photo
-    img, width, height = image_controller.get_image()
-
-    # Get random pixel point in the box
-    px, py = get_random_point(boite_prise)
-
-    # réalisation du crop
-    crop = img[py - HALF_CROP_HEIGHT : py + HALF_CROP_HEIGHT, px - HALF_CROP_WIDTH : px + HALF_CROP_WIDTH]
-
+    # Get all information from the camera
+    resp = coord_service('random', CROP_WIDTH, CROP_HEIGHT)
     # For debug
-    cv2.imshow("crop", crop)
+    cv2.imshow("rgb", resp.rgb)
+    cv2.imshow("depth", resp.depth)
     cv2.waitKey(1000)
-
-    goto_pixel_position(px, py)  # Move robot to pick position
-
-    # Lancement de l'action de prise
-    object_gripped = robot.pick(no_rotation=True)
-
-    # Si un objet est attrapé
+    # Move robot to pick position
+    pick_pose = pixel_to_pose(resp.xpick, resp.ypick)
+    object_gripped = robot.pick(pick_pose)
+    # If an object is gripped
     if object_gripped:
-
-        # création d'un point de lâcher aléatoire
-        px, py = get_random_point(boite_pose)
-
-
-        # on éteind la pompe
-        robot.send_gripper_message(False)
-
-        # on sauvegarde l'image crop dans le dossier success
-        image_path = (success_image_folder / ('success' + str(datetime.now()) + '.jpg')).resolve()
-        cv2.imwrite(str(image_path), crop)
-
+        # Place the object
+        place_pose = pixel_to_pose(resp.xplace, resp.yplace)
+        robot.place(place_pose)
+        save_images('success', resp.rgb, resp.depth)  # Save images in success folders
     else:
-
-        # on éteind la pompe
-        robot.send_gripper_message(False)
-
-        # on sauvegarde l'image crop dans le dossier fail
-        image_path = (fail_image_folder / ('fail' + str(datetime.now()) + '.jpg')).resolve()
-        cv2.imwrite(str(image_path), crop)
-
-    print("target reached")
-
+        robot.send_gripper_message(False)  # Switch off the gripper
+        save_images('fail', resp.rgb, resp.depth)  # Save images in fail folders
     # The robot must go out of the camera field
-    robot.go_to_initial_position(3)
-
-    cv2.destroyAllWindows()
+    robot.go_to_xyz_position(X_OUT, Y_OUT, Z_OUT)
+    #cv2.destroyAllWindows()
 
 
 
