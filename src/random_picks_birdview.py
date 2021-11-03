fc67ae301598af9a73f62a0780b085069716030a
ClementFumey31 <clement.fumey@2021.icam.fr>
2021-11-03 14:18:30 +0100

    Debug

--- src/random_picks_birdview.py
+++ src/random_picks_birdview.py
@@ -18,21 +18,24 @@ import rospy
 from datetime import datetime
 from pathlib import Path
 import sys
+from cv_bridge import CvBridge
 import cv2
 from raiv_camera_calibration.perspective_calibration import PerspectiveCalibration
 from raiv_libraries.robot_with_vaccum_gripper import Robot_with_vaccum_gripper
+from raiv_libraries.srv import get_coordservice, get_coordserviceResponse
 from raiv_libraries.robotUR import RobotUR
 import geometry_msgs.msg as geometry_msgs
 
 #
 # Constants
 #
-CROP_WIDTH = 224 # Width and height for rgb and depth cropped images
-CROP_HEIGHT = 224
+CROP_WIDTH = 25 # Width and height for rgb and depth cropped images
+CROP_HEIGHT = 25
 Z_PICK_PLACE = 0.1  # Z coord to start pick or place movement
-X_OUT = 0.2  # XYZ coord where the robot is out of camera scope
-Y_OUT = 0.1
+X_OUT = 0.0  # XYZ coord where the robot is out of camera scope
+Y_OUT = -0.3
 Z_OUT = 0.1
+bridge = CvBridge()
 
 def pixel_to_pose(px, py):
     """ Transpose pixel coord to XYZ coord (in the base robot frame) and return the corresponding frame """
@@ -71,7 +74,7 @@ robot.go_to_xyz_position(X_OUT, Y_OUT, Z_OUT) # Go out of camera scope
 # We can now ask a service to get and process 3D images
 coord_service_name = 'In_box_coordService'
 rospy.wait_for_service(coord_service_name)
-coord_service = rospy.ServiceProxy('coord_service_name', get_coordServiceResponse)
+coord_service = rospy.ServiceProxy(coord_service_name, get_coordservice)
 _ = coord_service('refresh', CROP_WIDTH, CROP_HEIGHT) # ask to refresh (i.e get a new image and find boxes)
 
 # Create, if they don't exist, <images_folder>/success/rgb, <images_folder>/success/depth,  <images_folder>/fail/rgb and <images_folder>/fail/depth folders
@@ -89,20 +92,28 @@ while True:
     # Get all information from the camera
     resp = coord_service('random', CROP_WIDTH, CROP_HEIGHT)
     # For debug
+
+    resp.rgb = bridge.imgmsg_to_cv2(resp.rgb, desired_encoding = 'passthrough')
+    resp.depth = bridge.imgmsg_to_cv2(resp.depth, desired_encoding = 'passthrough')
+    resp.depth = resp.depth * 100
+
     cv2.imshow("rgb", resp.rgb)
     cv2.imshow("depth", resp.depth)
     cv2.waitKey(1000)
+
     # Move robot to pick position
     pick_pose = pixel_to_pose(resp.xpick, resp.ypick)
     object_gripped = robot.pick(pick_pose)
     # If an object is gripped
     if object_gripped:
         # Place the object
+        print('Gripped')
         place_pose = pixel_to_pose(resp.xplace, resp.yplace)
+        print()
         robot.place(place_pose)
         save_images('success', resp.rgb, resp.depth)  # Save images in success folders
     else:
-        robot.send_gripper_message(False)  # Switch off the gripper
+        robot._send_gripper_message(False)  # Switch off the gripper
         save_images('fail', resp.rgb, resp.depth)  # Save images in fail folders
     # The robot must go out of the camera field
     robot.go_to_xyz_position(X_OUT, Y_OUT, Z_OUT)
