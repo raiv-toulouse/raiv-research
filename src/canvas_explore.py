8d006d6036a89ec44a437ade353ee452188d3e09
raiv-toulouse@icam.fr <raiv-toulouse>
2021-11-16 14:29:21 +0100

    Update explore.py to agree with the new librairies

--- src/canvas_explore.py
+++ src/canvas_explore.py
@@ -27,20 +27,24 @@ import rospy
 import rospkg
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
 from moveit_commander.conversions import pose_to_list
-from robot2 import Robot
+#from robot2 import Robot
 from ai_manager.Environment import Environment
 from ai_manager.Environment import Env_cam_bas
 from ai_manager.ImageController import ImageController
 import random
-from ur_icam_description.robotUR import RobotUR
+#from ur_icam_description.robotUR import RobotUR
+
+# WIDTH = HEIGHT = 56
+# dPoint = PerspectiveCalibration()
+# dPoint.setup_camera()
+# robot2 = Robot(Env_cam_bas)
+# rospy.init_node('explore2')
+# myRobot = RobotUR()
+# image_controller = ImageController(image_topic='/usb_cam2/image_raw')
+
 WIDTH = HEIGHT = 56
-dPoint = PerspectiveCalibration()
-dPoint.setup_camera()
-robot2 = Robot(Env_cam_bas)
-rospy.init_node('explore2')
-myRobot = RobotUR()
-image_controller = ImageController(image_topic='/usb_cam2/image_raw')
-class Canvas(QWidget):
+
+class CanvasExplore(QWidget):
 
     def __init__(self,parent):
         super().__init__(parent)
@@ -48,94 +52,52 @@ class Canvas(QWidget):
         self.pressed = self.moving = False
         self.previous_image = None
         self.all_preds = None
+        self.select_start = self.select_end = None
 
-    def set_image(self,filename):
-        self.image = QImage(filename)
+    def set_image(self,img):
+        self.image = QImage(img.tobytes("raw", "RGB"), img.width, img.height, QImage.Format_RGB888)  # Convert PILImage to QImage
         self.setMinimumSize(self.image.width(), self.image.height())
         self.previous_image = None
         self.all_preds = None
         self.update()
 
     def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
-            if self.previous_image:  # A new click, we restore the previous image without the rectangle
-                self.image = self.previous_image
-                self.setMinimumSize(self.image.width(), self.image.height())
-            self.pressed = True
-            self.center = event.pos()
-            self.update()
-
-    def mousePressEvent(self, event):
+        modifiers = QApplication.keyboardModifiers()
+        pos = event.pos()
         if event.button() == Qt.LeftButton:
-
-            pos = event.pos()
-            print(pos)
-
-            # enregistrement du crop au click
-
-            # # on prend la photo
-            # img, width, height = image_controller.get_image()
-            #
-            # # préparation de la variable de sauvegarde (nom du fichier, dossier de sauvegarde...)
-            # image_path = '{}/img{}.png'.format(  # Saving image
-            #     "{}/success".format(
-            #         '/home/student1/catkin_ws_noetic/src/bin_picking/ai_manager/src/ImageProcessing/image_camHaute/Update_images'),
-            #     # Path
-            #     "update")  # FIFO queue
-            #
-            # # sauvegarde de la photo
-            # img.save(image_path)
-            #
-            # # chemin d'accès à la photo prise
-            # path = r'/home/student1/catkin_ws_noetic/src/bin_picking/ai_manager/src/ImageProcessing/image_camHaute/Update_images/success/imgupdate.png'
-            #
-            # # chargement de la photo avec OpenCV
-            # frame = cv2.imread(path)
-            # center = [pos.x() + 112, pos.y() + 112]
-            # h = 224
-            # w = 224
-            # y = pos.y() - 112
-            # x = pos.x() - 112
-            # crop = frame[y:y + h, x:x + w]
-            #
-            # cv2.imshow("crop", crop)
-            # cv2.waitKey(1000)
-
-            image_coord = [pos.x(), pos.y()]
-            xyz = dPoint.from_2d_to_3d(image_coord)
-            print(xyz)  # ok
-            goal_x = -xyz[0][0] / 100
-            goal_y = -xyz[1][0] / 100
-
-            # calcul du déplacement à effectuer pour passer du point courant au point cible
-            move_x = goal_x - robot2.robot_commander.get_current_pose().pose.position.x
-            move_y = goal_y - robot2.robot_commander.get_current_pose().pose.position.y
-
-            # mouvement vers le point cible
-            robot2.relative_move(move_x, move_y, 0)
-
-            # Lancement de l'action de prise
-            object_gripped = robot2.take_pick(no_rotation=True)
-
-            self.parent.move_robot_to_take_pic()
-            robot2.send_gripper_message(False)
-
-            self.update()
-            self.parent._change_image()
+            if modifiers == Qt.ControlModifier:  # Ctrl + left click
+                self.parent.ask_robot_to_pick(pos.x(), pos.y())
+            else:
+                if self.previous_image:  # A new click, we restore the previous image without the rectangle
+                    self.image = self.previous_image
+                    self.setMinimumSize(self.image.width(), self.image.height())
+                self.pressed = True
+                self.center = event.pos()
+                self.update()
+        elif event.button() == Qt.RightButton:
+            self.select_start = pos
 
     def mouseMoveEvent(self, event):
-        if event.buttons() & Qt.RightButton:
+        if event.buttons() & Qt.LeftButton:
             self.moving = True
             self.center = event.pos()
             self.update()
+        elif event.buttons() & Qt.RightButton:
+            self.select_end = event.pos()
+            self.update()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.RightButton:
+        if event.button() == Qt.LeftButton:
             self.previous_image = self.image.copy()
             qp = QPainter(self.image)
             self._draw_rectangle(qp)
             self.pressed = self.moving = False
             self.update()
+        elif event.button() == Qt.RightButton:
+            if self.select_end:
+                self.parent.compute_map(self.select_start, self.select_end)
+                self.update()
+                self.select_start = self.select_end = None
 
     def paintEvent(self, event):
         qp = QPainter(self)
@@ -145,12 +107,20 @@ class Canvas(QWidget):
             self._draw_rectangle(qp)
         if self.all_preds:
             self._draw_pred(qp)
+        if self.select_end:
+            self._draw_selected_region(qp)
 
+    def _draw_selected_region(self, qp):
+        qp.setRenderHint(QPainter.Antialiasing)
+        qp.setPen(QPen(Qt.blue, 5))
+        w = self.select_end.x() - self.select_start.x()
+        h = self.select_end.y() - self.select_start.y()
+        qp.drawRect(self.select_start.x(), self.select_start.y(), w, h)
 
     def _draw_rectangle(self, qp):
         if self.parent.inference_model:  # A model exists, we can do inference
             x = self.center.x() - WIDTH / 2
-            y = self.center.y()-HEIGHT/2
+            y = self.center.y() - HEIGHT / 2
             qp.setRenderHint(QPainter.Antialiasing)
             qp.setPen(QPen(Qt.blue, 5))
             qp.drawPoint(self.center)
@@ -168,13 +138,17 @@ class Canvas(QWidget):
             qp.drawRect(x, y, WIDTH, HEIGHT)
 
     def _draw_pred(self, qp):
+        """ Display all predictions (green/red point + percentage of success) from self.all_preds list """
         threshold = self.parent.sb_threshold.value()
-        for (x, y , pred) in self.all_preds:
+        self.all_preds.sort(key=lambda pred: pred[2][0][1].item(), reverse=True)
+        for idx, (x, y , pred) in enumerate(self.all_preds):
             tensor_prob,tensor_cl = torch.max(pred, 1)
             if tensor_cl.item()==0 or (tensor_cl.item()==1 and tensor_prob.item()*100 < threshold):  # Fail
-                qp.setPen(QPen(Qt.red, 5))
+                qp.setPen(QPen(Qt.red, 5)) # Prediction under the threshold
             else:
-                qp.setPen(QPen(Qt.green, 5))
+                qp.setPen(QPen(Qt.green, 5))# Prediction above the threshold
+            if idx == 0:
+                qp.setPen(QPen(Qt.blue, 5))  # The best prediction
             qp.drawPoint(x, y)
             qp.setPen(Qt.black)
             qp.setFont(QFont('Decorative', 8))
@@ -183,6 +157,7 @@ class Canvas(QWidget):
             qp.drawText(x, y, text)
 
     def _compute_prob_and_class(self, pred):
+        """ Retrive class (success or fail) and its associeted percentage from pred """
         prob, cl = torch.max(pred, 1)
         if cl.item() == 0:  # Fail
             prob = 100 * (1 - prob.item())
