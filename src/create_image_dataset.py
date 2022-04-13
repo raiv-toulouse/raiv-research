20b1302e3b320308c9c5fdfe0d40a0b6a5559962
Philippe JUHEL <philippe.juhel@icam.fr>
2022-04-13 17:27:00 +0200

    Correction of some bugs due to get_coord_node improvments

--- src/create_image_dataset.py
+++ src/create_image_dataset.py
@@ -22,9 +22,9 @@ from pathlib import Path
 import sys
 import cv2
 from raiv_camera_calibration.perspective_calibration import PerspectiveCalibration
+from raiv_libraries.get_coord_node import InBoxCoord
 from raiv_libraries.robot_with_vaccum_gripper import Robot_with_vaccum_gripper
-from raiv_libraries.simple_image_controller import SimpleImageController
-from raiv_libraries.srv import get_coordservice, get_coordserviceResponse
+from raiv_libraries.srv import get_coordservice
 from raiv_libraries.robotUR import RobotUR
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -143,9 +143,9 @@ class CreateImageDataset(QWidget):
     def _set_image(self, px, py):
         bridge = CvBridge()
         """ Get an image from service and display it on the canvas """
-        resp = self.coord_service('fixed', CROP_WIDTH, CROP_HEIGHT, px, py)
-        self.rgb = resp.rgb
-        self.depth = resp.depth
+        resp = self.coord_service('fixed', InBoxCoord.PICK, InBoxCoord.IN_THE_BOX, CROP_WIDTH, CROP_HEIGHT, px, py)
+        self.rgb = resp.rgb_crop
+        self.depth = resp.depth_crop
         self.canvas.set_image(rospy.wait_for_message('/RGBClean', Image))
         self.depth = bridge.imgmsg_to_cv2(self.depth, desired_encoding='passthrough')
         self._histeq()
